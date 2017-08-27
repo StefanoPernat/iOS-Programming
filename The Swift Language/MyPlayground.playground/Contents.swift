1f4bd1093abd5399d6c6aedfebdfeb3ae43bdb65
Stefano Pernat <stefano.pernat@gmail.com>
2017-08-27 18:02:28 +0200

    initializers

--- The Swift Language/MyPlayground.playground/Contents.swift
+++ The Swift Language/MyPlayground.playground/Contents.swift
@@ -25,3 +25,22 @@ let countingUp = ["one", "two"]
 let nameByParkingSpace = [13: "Alice", 27: "Bob"]
 
 let secondElement = countingUp[1]
+
+// initializers
+let emptyString = String()
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+
+let defaultNumber = Int()
+let defaultBool = Bool()
+
+let meaningOfLife = String(number)
+
+let availableRooms: Set<Int> = [205, 411, 412]
+
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
