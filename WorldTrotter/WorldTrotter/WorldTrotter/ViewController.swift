64a156afc0cb02c6fb0ac4d65c58699f30fb8e60
Stefano Pernat <stefano.pernat@gmail.com>
2017-08-30 11:25:09 +0200

    deleted practice code

--- WorldTrotter/WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/WorldTrotter/ViewController.swift
@@ -10,27 +10,5 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // adding view programmatically: first create a frame
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        
-        // adding view programmatically: first create a view with the frame passed into the initializer
-        let firstView = UIView(frame: firstFrame)
-        
-        firstView.backgroundColor = UIColor.blue
-        
-        // adding view programmatically: adding the new view to the root view
-        view.addSubview(firstView)
-        
-        // let's create a second view
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.green
-        
-        firstView.addSubview(secondView)
-    }
 }
 
