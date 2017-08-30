c9a7333b4c6abef4f0929fdd11d70b1269a38e23
Stefano Pernat <stefano.pernat@gmail.com>
2017-08-30 18:05:49 +0200

    Replaced standard ViewController with ConversionViewController

---
+++ WorldTrotter/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  this view controller menages the conversion between fahrenheit and celsius,
+//  is the main viewController of the WorldTrotter App
+//
+//  Created by Stefano Pernat on 30/08/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
