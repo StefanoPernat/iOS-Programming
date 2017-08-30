a57ceee3ff5bad0e2d874904a57628e07ec70691
Stefano Pernat <stefano.pernat@gmail.com>
2017-08-30 11:10:07 +0200

    created my first view programmatically

--- WorldTrotter/WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/WorldTrotter/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // adding view programmatically: first create a frame
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        
+        // adding view programmatically: first create a view with the frame passed into the initializer
+        let firstView = UIView(frame: firstFrame)
+        
+        firstView.backgroundColor = UIColor.blue
+        
+        // adding view programmatically: adding the new view to the root view
+        view.addSubview(firstView)
+    }
 }
 
