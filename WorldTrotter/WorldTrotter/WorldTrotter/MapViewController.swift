9823b6c54d94f950d959aa14e704a3835256bd34
Stefano Pernat <stefano.pernat@gmail.com>
2017-09-01 16:27:48 +0200

    created MapView programmatically inside loadView

--- WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // when the MapViewController is created I will create a MapView programmatically
+        // and set this as the main view of MapViewController
+        mapView = MKMapView()
+        view = mapView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
