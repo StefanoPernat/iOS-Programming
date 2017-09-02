cd30941dbe38d11fab58e835f556c531ec7b158b
Stefano Pernat <stefano.pernat@gmail.com>
2017-09-02 19:04:34 +0200

    added custom class for WebViewController

---
+++ WorldTrotter/WorldTrotter/WorldTrotter/WebViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WebViewController.swift
+//  WorldTrotter
+//
+//  This is the view controller for the bronze challenge
+//
+//  Created by Stefano Pernat on 02/09/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    
+}
