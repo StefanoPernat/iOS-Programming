8385f9f1b6cd013e488e75cbcf3be9c9413b15b3
Stefano Pernat <stefano.pernat@gmail.com>
2017-09-28 20:02:52 +0200

    added buggy method to practice with debugger

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -24,6 +24,20 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: UIButton) {
         print("Called buttonTapped(_:)")
         print("Method: \(#function) in file: \(#file) line: \(#line) called")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        for _ in 0...10 {
+            array.removeObject(at: 0)
+        }
     }
 }
 
