6ba2d1e51990d4ce90db649480df114a4d729d52
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-01 19:00:35 +0200

    Added item class

---
+++ Homepwner/Homepwner/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Stefano Pernat on 01/10/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+}
