3be88f8b6f72aee9f70a942450f87aebc11c64b5
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-01 19:17:25 +0200

    Implemented convenience initializer for Item

--- Homepwner/Homepwner/Model/Item.swift
+++ Homepwner/Homepwner/Model/Item.swift
@@ -22,4 +22,25 @@ class Item: NSObject {
         
         super.init()
     }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            var idx = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjectives = adjectives[Int(idx)]
+            
+            idx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(idx)]
+            
+            let randomName = "\(randomAdjectives) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(UInt32(100)))
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
 }
