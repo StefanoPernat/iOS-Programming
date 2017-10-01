b363b62d2aa9a345cfbd9a3825ac5964de7545f8
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-01 18:51:03 +0200

    Add ItemsViewController class to the project

---
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Stefano Pernat on 01/10/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
