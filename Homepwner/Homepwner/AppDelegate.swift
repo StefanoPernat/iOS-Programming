a7aad6b9543ed77dac4a37af31d4d77e9f2fac0e
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-04 17:20:41 +0200

    set ItemsViewController's itemStore when the application launches

--- Homepwner/Homepwner/AppDelegate.swift
+++ Homepwner/Homepwner/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // create the itemStore
+        let itemStore = ItemStore()
+        
+        // retrive the root view controller (ItemsViewController) end set its itemStore property
+        let itemsController = window?.rootViewController as! ItemsViewController
+        itemsController.itemStore = itemStore
+        
         return true
     }
 
