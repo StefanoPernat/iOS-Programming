b8ce020a37ebe14d50807c3aeb72967757ee4d2a
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-04 17:01:51 +0200

    Added ItemStore class to the project (array of items)

---
+++ Homepwner/Homepwner/Model/ItemStore.swift
@@ -0,0 +1,13 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Stefano Pernat on 04/10/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+}
