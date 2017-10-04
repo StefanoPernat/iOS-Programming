d67801adc83df5f86f02ad7143f87eaf2fffdb88
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-04 17:07:28 +0200

    added createNewItem method to ItemStore class

--- Homepwner/Homepwner/Model/ItemStore.swift
+++ Homepwner/Homepwner/Model/ItemStore.swift
@@ -10,4 +10,12 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
+    
+    @discardableResult func createNewItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
 }
