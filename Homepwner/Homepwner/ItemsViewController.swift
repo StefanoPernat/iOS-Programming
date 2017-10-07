1ed8a19fb5b0ff992d13a9d86faedfaeacd912dd
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-07 11:59:02 +0200

    Added UIAlertController for a confirmation on delete

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -47,10 +47,26 @@ class ItemsViewController: UITableViewController {
         // if tableView is asking to commit a delete command...
         if editingStyle == .delete {
             let item = itemStore.allItems[indexPath.row]
-            // remove item from the store
-            itemStore.removeItem(item)
-            // remove the row from the table view
-            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            // prompt to the user an alert and ask him for confirmation while deleting
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let deleteAlertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {[unowned self] (action) -> Void in
+                // moved statements to remove item inside delete action closure
+                self.itemStore.removeItem(item)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            
+            //adding action to UIAlertController
+            deleteAlertController.addAction(cancelAction)
+            deleteAlertController.addAction(deleteAction)
+            
+            // present to the user the UIAlertController
+            present(deleteAlertController, animated: true, completion: nil)
         }
     }
     
