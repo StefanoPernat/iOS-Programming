ea866893ee74ff08911147de7a0908f5c9139d01
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-22 11:50:08 +0200

    Added custom UITableViewCell class

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Stefano Pernat on 22/10/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
