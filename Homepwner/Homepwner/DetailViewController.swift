eaea70e3db832fe5975077b2cc3c342091bada55
Stefano Pernat <stefano.pernat@gmail.com>
2017-10-27 19:16:31 +0200

    Connected Outlets from storyboard to DetailViewController

---
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Homepwner
+//
+//  Created by Stefano Pernat on 27/10/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    //IBoutlets
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var serialNumberField: UITextField!
+    @IBOutlet weak var valueField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
