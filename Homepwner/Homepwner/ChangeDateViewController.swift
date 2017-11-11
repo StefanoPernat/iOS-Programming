040bf0d90248fa864264a557f77909d6d5f74c19
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-11 09:20:21 +0100

    Added new VC for changing date and set up basic UI

---
+++ Homepwner/Homepwner/ChangeDateViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChangeDateViewController.swift
+//  Homepwner
+//
+//  Created by Stefano Pernat on 11/11/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ChangeDateViewController: UIViewController {
+    
+}
