27a9fc9e6c61619fe03e3e95af6036ad80d3c4f0
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-17 17:33:36 +0100

    Added imageKey property to Item class

--- Homepwner/Homepwner/Model/Item.swift
+++ Homepwner/Homepwner/Model/Item.swift
@@ -13,12 +13,14 @@ class Item: NSObject {
     var valueInDollars: Int
     var serialNumber: String?
     var dateCreated: Date
+    let itemKey: String
 
     init(name: String, serialNumber: String?, valueInDollars: Int) {
         self.name = name
         self.valueInDollars = valueInDollars
         self.serialNumber = serialNumber
         self.dateCreated = Date()
+        self.itemKey = UUID().uuidString
         
         super.init()
     }
