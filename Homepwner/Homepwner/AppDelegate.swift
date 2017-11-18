3f294609aa96e4ef7c4aea276a9429313fcedcbd
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-18 11:35:17 +0100

    Edit AppDelegate for saving items while entering background

--- Homepwner/Homepwner/AppDelegate.swift
+++ Homepwner/Homepwner/AppDelegate.swift
@@ -12,13 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let itemStore = ItemStore()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        // create the itemStore
-        let itemStore = ItemStore()
         
         // create an imageStore
         let imageStore = ImageStore()
@@ -41,6 +39,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let success = itemStore.saveChanges()
+        if success {
+            print("Saved all of the items")
+        } else {
+            print("Could not save any of the items")
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
