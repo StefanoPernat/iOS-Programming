30e1db15494c9e78cee2244c5ebc658f1fd6b7da
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-22 20:40:36 +0100

    Added Line struct

---
+++ TouchTracker/TouchTracker/Model/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Stefano Pernat on 22/11/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
