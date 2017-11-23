dc8538135218908b40c26bc7d70575ab4371bcab
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-23 20:30:44 +0100

    Overridden draw method

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -11,4 +11,29 @@ import UIKit
 class DrawView: UIView {
     var currentLine: Line?
     var finishedLines = [Line]()
+    
+    
+    func stroke(_ line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = 10
+        path.lineCapStyle = .round
+        
+        path.move(to: line.begin)
+        path.addLine(to: line.end)
+        path.stroke()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        // draw finished lines in black
+        UIColor.black.setStroke()
+        for line in finishedLines {
+            stroke(line)
+        }
+        
+        if let line = currentLine {
+            // if there is a line currently being drawn, do it in red
+            UIColor.red.setStroke()
+            stroke(line)
+        }
+    }
 }
