a2318508c4f0e724234c10d865d7b2e4cab2094f
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-23 10:13:05 +0100

    Created custom view for drawing and save drew lines

---
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Stefano Pernat on 22/11/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var currentLine: Line?
+    var finishedLines = [Line]()
+}
