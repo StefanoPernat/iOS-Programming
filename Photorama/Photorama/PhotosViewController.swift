781078f66886298cf0550ca401d3265cc5d0db0c
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-26 18:42:27 +0100

    Set up basic UI for PhotosVC and added first outlet

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Stefano Pernat on 26/11/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController : UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var imageView: UIImageView!
+}
