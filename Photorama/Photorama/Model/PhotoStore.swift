a5dd1a3f3615997e264aae5e71f185ec68cc64bd
Stefano Pernat <stefano.pernat@gmail.com>
2017-11-29 21:48:26 +0100

    update fetch methods inside PhotoStore

--- Photorama/Photorama/Model/PhotoStore.swift
+++ Photorama/Photorama/Model/PhotoStore.swift
@@ -19,26 +19,24 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
     
+    // MARK: - process data from webserver
+    private func processPhotoRequest(data: Data?, error: Error?) -> PhotoResult {
+        guard let jsonData = data else {
+            return .failure(error!)
+        }
+        
+        return FlickrAPI.photos(fromJSON: jsonData)
+    }
     
-    // MARK: - PhotoStore instance methods
-    func fetchInterestingPhotos() {
+    // MARK: - Fetching methods
+    func fetchInterestingPhotos(completition: @escaping (PhotoResult) -> Void) {
         let url = FlickrAPI.interestingPhotos
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
             
-            if let jsonData = data {
-                do {
-                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
-                    print(jsonObject)
-                } catch let error {
-                    print("Error creating JSON object: \(error)")
-                }
-            } else if let requestError = error {
-                print("Error fetching interesting photos: \(requestError)")
-            } else {
-                print("Unexpected error with the request")
-            }
+            let result = self.processPhotoRequest(data: data, error: error)
+            completition(result)
         }
         task.resume()
     }
