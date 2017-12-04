6d05985632e89ef1ac9d991215db6bba5d1baad4
Stefano Pernat <stefano.pernat@gmail.com>
2017-12-04 18:42:14 +0100

    Custom UICollectionViewCell

---
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Stefano Pernat on 03/12/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+}
