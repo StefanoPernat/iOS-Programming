b942bb4ebfb54b0ec986d6576b64cb604975a025
Stefano Pernat <stefano.pernat@gmail.com>
2017-12-04 18:52:39 +0100

    Implemented some utility methods

--- Photorama/Photorama/PhotoCollectionViewCell.swift
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -9,6 +9,31 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    // MARK: - Outlets
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    // MARK: - Helper methods
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    // MARK: - callback methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
 }
