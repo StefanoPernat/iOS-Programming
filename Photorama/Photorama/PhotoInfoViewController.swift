a2709e30e37230f7c7d6e55b0b2dd9fb34c9bd49
Stefano Pernat <stefano.pernat@gmail.com>
2017-12-06 21:51:07 +0100

    Implemented basic feature in PhotoInfoViewController

--- Photorama/Photorama/PhotoInfoViewController.swift
+++ Photorama/Photorama/PhotoInfoViewController.swift
@@ -11,4 +11,27 @@ import UIKit
 class PhotoInfoViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet var imageView: UIImageView!
+    
+    // MARK - Variables
+    var store: PhotoStore!
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) {
+            [unowned self] (result) -> Void in
+            
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
 }
