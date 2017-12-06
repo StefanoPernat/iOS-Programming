cbe168257ef55faaeaddab99ccffead7b0037755
Stefano Pernat <stefano.pernat@gmail.com>
2017-12-06 18:52:44 +0100

    Basic version of PhotoInfoViewController

---
+++ Photorama/Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Stefano Pernat on 05/12/17.
+//  Copyright © 2017 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet var imageView: UIImageView!
+}
